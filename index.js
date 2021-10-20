3565b481a5204df48dab593ef127bc0282e51e08
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-10-20 20:02:55 +1100

    Title: API server first response with test value.
    Description: For the first step I am implementing the Express server to
    generate and serve all managed data.
    The next step will be to form the original body of the response.

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const express=require('express');
 const app=express();
-const port=process.env.PORT || 3000;
+const port=process.env.PORT || 3001;
+
+app.get('/reciepts', (req,res)=>{
+    res.status(200).send({
+        name: 'This is a test responce.',
+        status: 'O.K'
+    })
+})
+app.listen(port,()=>{
+    console.log(`API Server is Running on ${port}`);
+})
