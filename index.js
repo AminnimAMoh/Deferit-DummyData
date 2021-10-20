fae41d789eb90e6c945b30b43cc2082b2e1cb938
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-10-20 20:28:59 +1100

    Started generating random data.
    Description: I have used a for loop to generate the receipts data.
    Within the loop, I am introducing the required values.
    Generating random dates was the first challenge and I found the solution
    in the following address:
    https://www.codegrepper.com/code-examples/javascript/generate+random+date+in+javascript

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-const express=require('express');
-const app=express();
-const jsonFile=require('jsonfile');
-const output='responce.json';
+const express = require("express");
+const app = express();
 
-const port=process.env.PORT || 3001;
+//jsonfile is a library to generate json files.
+const jsonFile = require("jsonfile");
+const output = "responce.json";
 
-app.get('/reciepts', (req,res)=>{
-    res.status(200).send({
-        name: 'This is a test responce.',
-        status: 'O.K'
-    })
-})
-app.listen(port,()=>{
-    console.log(`API Server is Running on ${port}`);
-})
+const port = process.env.PORT || 3001;
+
+const outputData = [{}];
+
+function generateRandomDate() {
+  //Solution from https://www.codegrepper.com/code-examples/javascript/generate+random+date+in+javascript
+  return new Date(
+    start.getTime() + Math.random * (end.getTime() - start.getTime())
+  );
+}
+for (let i = 0; i < 100; i++) {
+  const amount = "$" + parseInt(Math.random() * 1000);
+  const date = generateRandomDate();
+}
+
+app.get("/reciepts", (req, res) => {
+  res.status(200).send({
+    name: "This is a test responce.",
+    status: "O.K",
+  });
+});
+app.listen(port, () => {
+  console.log(`API Server is Running on ${port}`);
+});
