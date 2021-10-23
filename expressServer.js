d07de15690c778cfedcd215d3b28ecadef47ad7e
AminnimAMoh <43339115+AminnimAMoh@users.noreply.github.com>
2021-10-23 11:46:21 +1100

    Adding a middleware to perform all paginations before starting the requests.
    As I have only one request method GET here this step was not necessary but I have done it to learn and see leave space for improvements.
    The paginations solution is found from https://youtu.be/ZX3qt0UWifc.

--- expressServer.js
+++ expressServer.js
@@ -1,21 +1,52 @@
-const express=require('express')
-const app=express();
-const port=process.env.PORT || 3001;
-const data=require('./responce.json');
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3001;
+const data = require("./responce.json");
 
 // http://localhost:3001/responce?page=1&limit=5
 
-app.get('/responce', (req,res)=>{
-    const page=req.query.page;
-    const limit=req.query.limit;
+// Pagination solution find from:
+// https://youtu.be/ZX3qt0UWifc
+app.get("/responce", paginatedResponce(data), (req, res) => {
+  res.json(res.paginatedResults);
+});
 
-    const startIndex=(page-1)*limit;
-    const endIndex=page*limit;
+//This is a middleware to trigger before any other functions in this process.
+//PaginatedResponce as a middleware allow us to use the same structure for many different methods such as
+//Post, Put, etc.
+function paginatedResponce(model) {
+  //Like any other middleware we need to have req, res, next.
+  return (req, res, next) => {
+    const page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit);
 
-    const constrainData=data.slice(startIndex, endIndex);
-    res.json(constrainData)
-})
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
 
-app.listen(port, ()=>{
-    console.log('I am Listening');
-})
+    const results = {};
+
+    if (endIndex < model.length) {
+      results.next = {
+        page: page + 1,
+        limit: limit,
+      };
+    }
+
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit: limit,
+      };
+    }
+
+    results.responce = model.slice(startIndex, endIndex);
+    res.paginatedResults = results;
+
+    //Next pass us to call the next function.
+    next();
+  };
+}
+
+app.listen(port, () => {
+  console.log("I am Listening");
+});
